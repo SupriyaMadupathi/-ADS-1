d80d9ce2f41538357929bfa7cdd028f17eee4786
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-09-25 19:31:53 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- Assignments/m2/CodeCamp_Percolation/CodeCamp_Percolation/Solution.java
+++ Assignments/m2/CodeCamp_Percolation/CodeCamp_Percolation/Solution.java
@@ -1,85 +1,107 @@
-import java.util.*;
-// public class percocolation {
-//    public percocolation(int n)                // create n-by-n grid, with all sites blocked
-//    public    void open(int row, int col)    // open site (row, col) if it is not open already
-//    public boolean isOpen(int row, int col)  // is site (row, col) open?
-//    public boolean isFull(int row, int col)  // is site (row, col) full?
-//    public     int numberOfOpenSites()       // number of open sites
-//    public boolean percocolates()              // does the system percocolate?
-// }
+import java.util.Scanner;
+/**
+ * Class for percolation.
+ */
+class Percolation {
+	/**
+	 * { var_description }
+	 */
+	private WeightedQuickUnionUF wqf;
+
+	private int n;
+	private int size;
+	private int row;
+	private int col;
+	private boolean[] connected;
+	private int count;
+	// create n-by-n grid, with all sites blocked
+   public Percolation(int n1) {
+   	this.n = n1;
+   	this.count = 0;
+   	this.size = n1 * n1;
+   	this.row = size;
+   	this.col = size + 1;
+   	connected = new boolean[size];
+   	wqf = new WeightedQuickUnionUF(size + 2);
+   	for (int i = 0; i < n; i++) {
+   		wqf.union(row, i);
+   		wqf.union(col, size - i - 1);
+   	}
+   }   
+
+   public int component(int i, int j) {
+   		return n * (i - 1) + j - 1;
+   }
+
+   private void LinkOpenSites(int row, int col) {
+   	if (connected[col] && !wqf.connected(row, col))
+   	{
+   		wqf.union(row, col);
+   	}
+   }
+
+   public int numberOfOpenSites() {
+   	return count;
+   }       
+   public void open(int row, int col) {
+   	int index = component(row, col);
+   	connected[index] = true;
+   	count++;
+   	int bottom = index + n;
+   	int top = index - n;
+   	if (n == 1) {
+   		wqf.union(row, index);
+   		wqf.union(col, index);
+
+   	}
+   	if (bottom < size) {
+   		LinkOpenSites(index, bottom);
+   	} 
+   	if (top >= 0) {
+   		LinkOpenSites(index, top);
+   	}
+   	if(col == 1) {
+   		if(col != n) {
+   			LinkOpenSites(index, index + 1);
+   		}
+   		return;
+   	} if (col == n) {
+   		LinkOpenSites(index, index - 1);
+   		return;
+   	}
+   	LinkOpenSites(index, index + 1);
+   	LinkOpenSites(index, index - 1);
 
 
-// You can implement the above API to solve the problem
- class percocolation {
-		int[][] grid;
-		int openSite;
-		WeightedQuickUnionUF wqu;
-		int n;
-	 percocolation(int n)
-	{
-		grid = new int[n][n];
-		openSite = 0;
-		wqu = new WeightedQuickUnionUF((n*n)+2);
-		n = n;
-	}
-	public void open(int row, int col){
-		row = row -1;
-		col = col -1;
-		grid[row][col] = 1;
-		openSite++;
-		if (row == 0) {
-			wqu.union(0, component(row, col));
-		}
-		if (row == n -1) {
-			wqu.union(((n*n)+1), component(row, col));			
-		}
-		if (row >= 1) {
-			if (grid[row+1][col] == 1) {
-				wqu.union(component(row,col), component(row+1,col));
-			}
-		}
+   } 
+   public boolean isOpen(int row, int col) {
+   	return connected[component(row, col)];
+   }
+   public boolean percolates() {
+   	return wqf.connected(row, col);
 
-		if (row <= n-1) {
-			if (grid[row-1][col] == 1) {
-				wqu.union(component(row,col), component(row-1,col));
-			}
-		}
-		if (col >=1) {
-			if (grid[row][col+1] == 1) {
-				wqu.union(component(row,col), component(row,col+1));
-			}
-		}
-		if (col <= n-1) {
-			if (grid[row][col-1] == 1) {
-				wqu.union(component(row,col), component(row,col-1));
-			}
-		}
-	}
-	public int component(int a, int b) {
-		return ((a*n)+b)+1;
-	}
-	public boolean isOpen(int row, int col) {
-		return grid[row-1][col-1] == 1;
-	}
-	public boolean isFull(int row, int col) {
-		return grid[row-1][col-1] == 0;
-	}
-	public int numberOfOpenSites() {
-		return openSite;
-	}
-	public boolean percocolates() {
-		return wqu.connected(0, (n*n)+1);
-	}
-
+   }             
 }
-public class Solution {
+
+
+
+public final class Solution {
+
+	private Solution() {
+
+	}
+
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int size = s.nextInt();
-		percocolation perco = new percocolation(size);
-		while (s.hasNext()) {
-			perco.open(s.nextInt(), s.nextInt());
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.nextLine());
+		Percolation p = new Percolation(n);
+		// int i = 0;
+		while(sc.hasNext()) {
+			String [] tokens = sc.nextLine().split(" ");
+			p.open(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+			// i++;
 		}
-		System.out.println(perco.percocolates());
+		System.out.println(p.percolates() && p.numberOfOpenSites() != 0);
+
 	}
 }
