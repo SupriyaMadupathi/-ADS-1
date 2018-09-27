bf83ab61df9d4cc96c4ad63f2bc6fdde8f892574
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-09-27 20:51:37 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ Assignments/m4/4.1 Balanced Paranthesis/4.1 Balanced Paranthesis/Solution.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+class Stack {
+    
+    class Node {
+        
+        private Node next;
+        private char item;
+    }
+    private Node head = null;
+    public void Push(final char s) {
+        Node ll = head;
+        head = new Node();
+        head.item = s;
+        head.next = ll;
+    }
+    
+    public char Pop() {
+        if (head == null) {
+            return 's';
+        }
+        
+        char m = head.item;
+        head = head.next;
+        return m;
+    }
+    public Node gethead() {
+        return head;
+    }
+    public String toString() {
+        String str = "";
+        Node fun = head;
+        while (fun != null) {
+            str += fun.item;
+            fun = fun.next;
+        }
+        return str;
+    }
+}
+class Balanced {
+    
+    Balanced() {
+    }
+    public boolean balance(final String str) {
+        Stack sc = new Stack();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '[' || str.charAt(i) == '('
+                || str.charAt(i) == '{') {
+                sc.Push(str.charAt(i));
+            } else {
+                char ch = sc.Pop();
+                if ((ch == '(' && str.charAt(i) == ')') || (ch == '['
+                    && str.charAt(i) == ']') || (ch == '{'
+                    && str.charAt(i) == ')')) {
+                    
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return sc.gethead() == null;
+    }
+}
+
+public final class Solution {
+    
+    private Solution() {
+        
+    }
+    
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        String[] strarray = new String[n];
+        for (int i = 0; i < n; i++) {
+            strarray[i] = scan.nextLine();
+        }
+        Balanced brace = new Balanced();
+        for (int i = 0; i < n; i++) {
+            if (brace.balance(strarray[i])) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
+
