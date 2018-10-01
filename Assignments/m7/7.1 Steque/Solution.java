3fd32944d16eba7b84b2235e70d9d348a170ff0e
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-10-01 19:12:05 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- Assignments/m7/7.1 Steque/Solution.java
+++ Assignments/m7/7.1 Steque/Solution.java
@@ -1,50 +1,50 @@
 import java.util.Scanner;
 
 class Node {
-	String data;
-	Node next;
-	Node() {
+    String data;
+    Node next;
+    Node() {
 
-	}
-	Node(String data1) {
-		this.data = data1;
-	}
+    }
+    Node(String data1) {
+        this.data = data1;
+    }
 }
 /**
  * Class for steque.
  */
 class Steque{
-	/**
+    /**
      * start.
      */
-	private Node start;
-	/**
+    private Node start;
+    /**
      * end.
      */
-	private Node end;
-	/**
+    private Node end;
+    /**
      * size.
      */
-	private int size;
-	/**
+    private int size;
+    /**
      * Constructs the object.
      */
-	Steque() {
+    Steque() {
 
-		size = 0;
-		start = new Node();
-		 end = new Node();
+        size = 0;
+        start = new Node();
+         end = new Node();
 
-	}
-	/**
-	 * { method to push elements}.
-	 *
-	 * @param      item  The item
-	 */
-	void push(String item) {
+    }
+    /**
+     * { method to push elements}.
+     *
+     * @param      item  The item
+     */
+    void push(String item) {
 
-		//System.out.println("BHOOM");
-		Node oldstart = new Node(item);
+        //System.out.println("BHOOM");
+        Node oldstart = new Node(item);
         if (size == 0) {
             start.data = item;
             start.next = null;
@@ -76,7 +76,7 @@ class Steque{
             end.next = null;
             end.next = oldend;
             end = oldend;
-            size++; 
+            size++;
     }
     /**
      * { function_description }
@@ -87,89 +87,95 @@ class Steque{
      */
     String pop() throws Exception{
 
-    	if (size == 0) {
-    		throw new Exception("Steque is empty.");
-    	}
-		String data = start.data;
-		start = start.next;
-		size--;
-		return data;
-	}
-	/**
-	 * Determines if empty.
-	 *
-	 * @return     True if empty, False otherwise.
-	 */
-	boolean isEmpty(){
+        if (size == 0) {
+            throw new Exception("Steque is empty.");
+        }
+        String data = start.data;
+        start = start.next;
+        size--;
+        return data;
+    }
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
+    boolean isEmpty(){
 
-		if (size==0) {
-			return true;
-		}else 
-		return false;
-	}
-	/**
-	 * Returns a string representation of the object.
-	 * 
-	 * @return     String representation of the object.
-	 */
-	public String toString() {
+        if (size==0) {
+            return true;
+        }else 
+        return false;
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
 
-		if (size != 0) {
-			String str = "";
+        if (size != 0) {
+            String str = "";
             Node temp = start;
             while (temp != null) {
                 str += temp.data + ", ";
                 temp = temp.next;
             }
             return str.substring(0, str.length() - 2);
-		}
-        	//System.out.println("Helle");
+        }
+            //System.out.println("Helle");
       return "[]";
     }
 }
 /**
  * Class for solution.
  */
-class Solution {
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //constrcutor.
+    }
 
-	/**
-	 * { main method}.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
+    /**
+     * { main method}.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
 
-		Scanner s = new Scanner(System.in);
-		int lines =  Integer.parseInt(s.nextLine());
-		Steque sq = new Steque();
-		while (s.hasNext()) {
-			String[] tokens = s.nextLine().split(" ");
-			switch (tokens[0]) {
-				case "push":
-				sq.push(tokens[1]);
-				System.out.println(sq.toString());
-				break;
-				case "pop":
-				try {
-			        sq.pop();
-			        if (!sq.isEmpty()) {
-			            System.out.println(sq);
-			        } else {
-			        	System.out.println("Steque is empty.");
-			        }
-					} catch (Exception e) {
-					System.out.println(e.getMessage());
-			}
-			break;
-				case "enqueue":
-				sq.enque(tokens[1]);
-				System.out.println(sq);
-				break;
-				default :
-				sq = new Steque();
-				System.out.println();
-				break;
-			}
-	}
+        Scanner s = new Scanner(System.in);
+        int lines =  Integer.parseInt(s.nextLine());
+        Steque sq = new Steque();
+        while (s.hasNext()) {
+            String[] tokens = s.nextLine().split(" ");
+            switch (tokens[0]) {
+                case "push":
+                sq.push(tokens[1]);
+                System.out.println(sq.toString());
+                break;
+                case "pop":
+                try {
+                    sq.pop();
+                    if (!sq.isEmpty()) {
+                        System.out.println(sq);
+                    } else {
+                        System.out.println("Steque is empty.");
+                    }
+                    } catch (Exception e) {
+                    System.out.println(e.getMessage());
+            }
+            break;
+                case "enqueue":
+                sq.enque(tokens[1]);
+                System.out.println(sq);
+                break;
+                default :
+                sq = new Steque();
+                System.out.println();
+                break;
+            }
+    }
 }
 }
