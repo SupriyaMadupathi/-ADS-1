c8e8c9cc1b4421c4bed0fb48a3d2def1bb44e3b0
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-10-02 22:57:07 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- Assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ Assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -4,25 +4,67 @@ class Ranking {
 	int win;
 	int loss;
 	int draw;
+	/**
+     * Constructs the object.
+     * time complexity is 1.
+     * @param      n     { country name }.
+     * @param      w     { no of wins }.
+     * @param      l     { no of losses }.
+     * @param      d     { no of draws }.
+     */
 	Ranking(String n, int w, int l, int d) {
+
 		this.name = n;
 		this.win = w;
 		this.loss = l;
 		this.draw = d;
+
 	}
+	/**
+	 * Gets the name.
+	 *
+	 * @return     The name.
+	 */
 	String getName(){
+
 		return this.name;
 	}
+	/**
+	 * Gets the win.
+	 *
+	 * @return     The win.
+	 */
 	int getWin() {
+
 		return this.win;
 	}
+	/**
+	 * Gets the loss.
+	 *
+	 * @return     The loss.
+	 */
 	int getLoss() {
+
 		return this.loss;
 	}
+	/**
+	 * Gets the draw.
+	 *
+	 * @return     The draw.
+	 */
 	int getDraw() {
+
 		return this.draw;
 	}
+	/**
+	 * {method to compare elements}.
+	 *
+	 * @param      that  The that
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public int compareTo(final Ranking that) {
+
         if (this.getWin() < that.getWin()) {
             return -1;
         } else if (this.getWin() > that.getWin()) {
@@ -43,30 +85,67 @@ class Ranking {
         return 0;
     }
     }
+    /**
+     * Class for sorting.
+     */
     class Sorting {
     private final int twenty = 20;
     private Ranking[] team;
     private int size;
+    /**
+     * Constructs the object.
+     * time complexity is 1.
+     */
     Sorting() {
+
         team = new Ranking[twenty];
         size = 0;
     }
+    //takes O(n) time complexity
+    /**
+     * { method for adding }.
+     *
+     * @param      r     { parameter_description }
+     */
     public void add(final Ranking r) {
+
         team[size++] = r;
     }
+    //takes O(n) time complexity.
+    /**
+     * { method for swapping }.
+     *
+     * @param      a     { parameter_description }
+     * @param      i     { parameter_description }
+     * @param      min   The minimum
+     */
     void exchange(Ranking[] a, int i, int min) {
+
 		Ranking temp = a[min];
 		a[min] = a[i];
 		a[i] = temp;
 	}
+	//takes O(n^2) time complexity
+	/**
+	 * Returns a string representation of the object.
+	 *
+	 * @return     String representation of the object.
+	 */
 	public String toString() {
+
         String s = "";
         for (int i = 0; i < size; i++) {
             s += team[i].getName() + ",";
         }
         return s.substring(0, s.length() - 1);
     }
+
+	//takes O(n^2) time complexity.
+    /**
+     * {method for insertion sort }
+     */
     void InsertionSort() {
+
 		for(int i = 0; i < size-1; i++) {
 			int min = i; 
 			for(int j = i+1; j > 0; j--) {
@@ -78,18 +157,39 @@ class Ranking {
 			
 		}
 	}
-	//takes O(1) time complexity.
+	
+	// takes O(1) time complexity.
+	/**
+	* @param      teams  The teams
+	* @param      j      { parameter_description }
+	* @param      min    The minimum
+	*
+	* @return     { description_of_the_return_value }
+	*/
 	boolean less(Ranking[] teams, int j, int min) {
+
 		return team[j].compareTo(team[min]) > 0;
 	}
 }
+/**
+ * this is a solution class.
+ */
 public final class Solution {
     
+    /**
+     * Constructs the object.
+     */
     private Solution() {
         
     }
     
+    /**
+     * { main function}.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
+
         Scanner sc = new Scanner(System.in);
         Sorting sort = new Sorting();
         final int one = 1;
