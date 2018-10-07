a569a74238ec750c56f613b995927607f5f8f305
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-10-07 21:12:11 +0530

    "ads1_m11_a1_v2: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Assignments/m11/11.1 Cutoff Quicksort to Insertion Sort - updated/Solution.java
@@ -0,0 +1,186 @@
+import java.util.Scanner;
+/**.
+ * { item_description }
+ */
+/**
+ * Class for sorting.
+ */
+class Sorting {
+
+    /**.
+     * Constructs the object.
+     */
+    Sorting() {
+        /**.
+         * { item_description }
+         */
+    }
+// this method takes the complexity of n^2.
+// time complexity for this method is O(n^2).
+// In first for loop and second loop it iterates through the size of array
+/**.
+     * { function_description }
+     *
+     * @param      arr  The arguments
+     * @param      low  The arguments
+     * @param      high  The arguments
+     */
+    public void insertion(final Comparable[] arr, final int low,
+     final int high) {
+        for (int i = low; i <= high; i++) {
+            for (int j = i; j > low && less(arr[j], arr[j - 1]); j--) {
+                swap(arr, j, j - 1);
+            }
+        }
+    }
+// this method has comstant time complexity.
+// time complexity of this method is O(1).
+/**.
+     * {  It swaps the elements only once. }
+     *
+     * @param      arr  The arguments
+     * @param      j  The arguments
+     * @param      min  The arguments
+     */
+    public void swap(final Comparable[] arr, final int j, final int min) {
+        Comparable temp = arr[j];
+        arr[j] = arr[min];
+        arr[min] = temp;
+    }
+    //this method has the time complexity of n.
+// time complexity of this method is O(n). It iterates through
+// the array to print all the objects in array.
+
+    /**.
+     * Returns a string representation of the object.
+     *
+     * @param      arr   The arr
+     *
+     * @return     String representation of the object.
+     */
+    public String toString(final Comparable[] arr) {
+        String str = "[";
+        for (int i = 0; i < arr.length - 1; i++) {
+            str += arr[i] + ", ";
+        }
+        str += arr[arr.length - 1] + "]";
+        return str;
+    }
+    // this method has the complexity of n.
+// time complexity is O(log n)
+/**.
+     * { function_description }
+     *
+     * @param      arr  The arguments
+     * @param      low  The arguments
+     * @param      high  The arguments
+     * @param      cutOff  The arguments
+     */
+    public void sort(final Comparable[] arr, final int low,
+     final int high, final int cutOff) {
+        if (high <= low + cutOff - 1) {
+            insertion(arr, low, high);
+            System.out.println("insertionSort called");
+            return;
+        }
+        int k = partition(arr, low, high);
+        System.out.println(toString(arr));
+        sort(arr, low, k - 1, cutOff);
+        sort(arr, k + 1, high, cutOff);
+    }
+    //this method has constant time complexity
+//time complexity is O(1)
+
+    /**.
+     * { function_description }
+     *
+     * @param      array   The array
+     * @param      cutOff  The cut off
+     */
+    public void sort(final Comparable[] array, final int cutOff) {
+        sort(array, 0, array.length - 1, cutOff);
+    }
+    //this method has complexity of n.
+// time complexity is O(n)
+
+    /**.
+     * { function for partitioning }
+     *
+     * @param      arr   The arr
+     * @param      low   The low
+     * @param      high  The high
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int partition(final Comparable[] arr,
+     final int low, final int high) {
+        int i = low;
+        int j = high + 1;
+        while (true) {
+            while (less(arr[++i], arr[low])) {
+                if (i == high) {
+                    break;
+                }
+            }
+            while (less(arr[low], arr[--j])) {
+                if (j == low) {
+                    break;
+                }
+            }
+            if (i >= j) {
+                break;
+            }
+            swap(arr, i, j);
+        }
+        swap(arr, low, j);
+        // System.out.println(toString(arr));
+        return j;
+    }
+    // this method has constant time complexity.
+// time complexity is O(1)
+
+    /**.
+     * {this method returns boolean value }
+     *
+     * @param      a     { parameter_description }
+     * @param      b     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean less(final Comparable a, final Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+}
+
+public final class Solution {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        /**.
+         * { item_description }
+         */
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Sorting sort = new Sorting();
+        int testcases = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < testcases; i++) {
+            int j = Integer.parseInt(sc.nextLine());
+            // sc.nextLine();
+            String line = sc.nextLine();
+            String[] tokens = line.split(" ");
+            if (tokens[0].equals("")) {
+                System.out.println("[]");
+            } else {
+                sort.sort(tokens, j);
+                System.out.println(sort.toString(tokens));
+            }
+        }
+    }
+}
