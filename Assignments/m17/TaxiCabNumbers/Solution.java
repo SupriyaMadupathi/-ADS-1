b2183f2e3be40acd50184767200ff3d73f68e6f1
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-10-12 18:39:31 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m17/TaxiCabNumbers/Solution.java
+++ Assignments/m17/TaxiCabNumbers/Solution.java
@@ -1,17 +1,24 @@
 import java.util.*;
-
+/**
+ * Class for solution.
+ */
 class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		while(sc.hasNextLine()){
-			String[] input = sc.nextLine().split(" ");
-			int N = Integer.parseInt(input[0]);
-			int M = Integer.parseInt(input[1]);
-			int n = 1000;
-			MinPQ<Taxicab> pq = new MinPQ<Taxicab>();
-	        	for (int i = 1; i <= n; i++) {
-	            pq.insert(new Taxicab(i, i));
-        	}
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()){
+            String[] input = sc.nextLine().split(" ");
+            int N = Integer.parseInt(input[0]);
+            int M = Integer.parseInt(input[1]);
+            int n = 1000;
+            MinPQ<Taxicab> pq = new MinPQ<Taxicab>();
+                for (int i = 1; i <= n; i++) {
+                pq.insert(new Taxicab(i, i));
+            }
         int pair = 1;
         Taxicab previous = new Taxicab(0, 0);
         int match = 0;
@@ -20,12 +27,12 @@ class Solution {
             if (previous.sum == curr.sum) {
                 pair++;
                 if (pair == M){
-                	match = match+1;
+                    match = match+1;
                 }
                 if(match == N){
-	            	System.out.println(previous.sum);
-	            	break;
-            	}
+                    System.out.println(previous.sum);
+                    break;
+                }
             }
             else {
                 pair = 1;
@@ -34,5 +41,5 @@ class Solution {
             if (curr.j < n) pq.insert(new Taxicab(curr.i, curr.j + 1));
         }
     }
-	}
-	}
+    }
+    }
