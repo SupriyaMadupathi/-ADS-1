9b42dd1bb0ca9be169689296be1aabfba3bd2f72
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-10-12 18:47:34 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m17/TaxiCabNumbers/Solution.java
+++ Assignments/m17/TaxiCabNumbers/Solution.java
@@ -1,45 +1,53 @@
-import java.util.*;
+import java.util.Scanner;
 /**
  * Class for solution.
  */
-class Solution {
+final class Solution {
     /**
-     * { function_description }
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * main function.
      *
      * @param      args  The arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNextLine()){
-            String[] input = sc.nextLine().split(" ");
-            int N = Integer.parseInt(input[0]);
-            int M = Integer.parseInt(input[1]);
-            int n = 1000;
+        final int thousand = 1000;
+        while (sc.hasNextLine()) {
+            String[] token = sc.nextLine().split(" ");
+            int ip1 = Integer.parseInt(token[0]);
+            int ip2 = Integer.parseInt(token[1]);
+            int n = thousand;
             MinPQ<Taxicab> pq = new MinPQ<Taxicab>();
                 for (int i = 1; i <= n; i++) {
                 pq.insert(new Taxicab(i, i));
             }
-        int pair = 1;
-        Taxicab previous = new Taxicab(0, 0);
-        int match = 0;
-        while (!pq.isEmpty()) {
-            Taxicab curr = pq.delMin();
-            if (previous.sum == curr.sum) {
-                pair++;
-                if (pair == M){
-                    match = match+1;
+            int pair = 1;
+            Taxicab previous = new Taxicab(0, 0);
+            int match = 0;
+            while (!pq.isEmpty()) {
+                Taxicab cs = pq.delMin();
+                if (previous.sum == cs.sum) {
+                    pair++;
+                    if (pair == ip2) {
+                        match = match + 1;
+                    }
+                    if (match == ip1) {
+                        System.out.println(previous.sum);
+                        break;
+                    }
+                } else {
+                    pair = 1;
                 }
-                if(match == N){
-                    System.out.println(previous.sum);
-                    break;
+                previous = cs;
+                if (cs.j < n) {
+                    pq.insert(new Taxicab(cs.i, cs.j + 1));
                 }
             }
-            else {
-                pair = 1;
-            }
-            previous = curr;
-            if (curr.j < n) pq.insert(new Taxicab(curr.i, curr.j + 1));
         }
     }
-    }
-    }
+}
