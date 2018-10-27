73208528259e78cf8b3e2bbd172e7ae71466ebfb
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-10-27 11:59:52 +0530

    "m24_a1_1: 2/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m24/1/Solution.java
+++ Assignments/m24/1/Solution.java
@@ -1,62 +1,73 @@
 import java.util.Scanner;
 
-class Student  {
-	 String name;
-	 int roll_number;
-	 double marks;
-
-	 Student(){
-
-	 }
-	 Student(String name1, int roll1, double marks1){
-	 	this.name= name1;
-	 	this.roll_number = roll1;
-	 	this.marks = marks1;
-	 }
-	 public String getName(){
-	 	return name;
-	 }
-	 public int getRoll_number(){
-	 	return roll_number;
-	 }
-	 public double getMarks(){
-	 	return marks;
-	 }
-	 
+class Student {
+    
+    private String roll_number;
+    
+    private String name;
+    
+    private double marks;
+    
+    Student() {
+    }
+    
+    Student(final String roll_number1, final String name1, final double marks1) {
+        roll_number = roll_number1;
+        name = name1;
+        marks = marks1;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String gerRollnumber() {
+        return roll_number;
+    }
+    
+    public Double getTotal() {
+        return marks;
+    }
 }
-class Solution {
-	Solution(){
 
-	}
-	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
-		Student details = new Student();
-		LinearProbingHashST<String, Integer> l1 = new LinearProbingHashST();
-		LinearProbingHashST<String, Double> l2 = new LinearProbingHashST();
-        int lines = Integer.parseInt(sc.nextLine());
-        for (int i=0; i<lines;i++){
-        	String stu = sc.nextLine();
-        	String[] tokens = stu.split(",");
-        	details = new Student(tokens[0], Integer.parseInt(tokens[1]),
+final class Solution {
+    
+    private Solution() {
+
+    }
+    
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Student details = new Student();
+        LinearProbingHashST<String, String> l1 = new LinearProbingHashST();
+        LinearProbingHashST<String, Double> l2 = new LinearProbingHashST();
+        int range = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < range; i++) {
+             String stu = sc.nextLine();
+             String[] tokens = stu.split(",");
+             details = new Student(tokens[0], tokens[1],
                 Double.parseDouble(tokens[2]));
-             l1.put(tokens[0], Integer.parseInt(tokens[1]));
+             l1.put(tokens[0], tokens[1]);
              l2.put(tokens[0], Double.parseDouble(tokens[2]));
             }
-            int n = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i < n; i++) {
-            String que = sc.nextLine();
-            String[] query = que.split(" ");
+        int m = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < m; i++) {
+            String stu = sc.nextLine();
+            String[] query = stu.split(" ");
+            
             switch (query[2]) {
                 case "1":
                     System.out.println(l1.get(query[1]));
+
+                
                 break;
                 case "2":
                 System.out.println(l2.get(query[1]));
+
                 break;
                 default:
                 break;
             }
         }
-
-	}
+    }
 }
