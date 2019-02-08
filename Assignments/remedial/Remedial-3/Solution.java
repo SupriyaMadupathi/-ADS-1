0c046a39eaeaee7c1a6fc9730e269ad550555014
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2019-02-08 11:45:08 +0530

    "ads1_remedial_3_a1_1: 0/7 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Assignments/remedial/Remedial-3/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public final class Solution {
+    private Solution() {
+
+    }
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BinarySearchTree<String, Double> bst = new BinarySearchTree();
+        int m = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i< m; i++) {
+        	String stu = sc.nextLine();
+        	String[] data = stu.split(",");
+        	System.out.println(data);
+        	String key = new String(data[1]);
+        	double value = Double.parseDouble(data[2]);
+        	bst.put(key, value);
+        }
+        // int n = Integer.parseInt(sc.nextLine());
+        // for(int i = 0; i< n; i++){
+        // 	String stu = sc.nextLine();
+
+        // }
+    }
+}
+
