9666540d25c81c95dc6b628eb1be1576de377596
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2019-02-08 18:54:01 +0530

    "ads1_remedial_3_a1_1: 0/7 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/remedial/Remedial-3/Solution.java
+++ Assignments/remedial/Remedial-3/Solution.java
@@ -86,6 +86,7 @@ class Sorting {
         String s = "";
         for (int i = 0; i < size; i++) {
             s += marks[i].getRollno() + "," + marks[i].getName() + "," + marks[i].getMarks();
+            break;
         }
         return s;
     }
