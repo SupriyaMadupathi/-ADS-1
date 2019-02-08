cbd4c85ef4cd13489dcd060c5d5631b8fae371f7
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2019-02-08 12:55:57 +0530

    "ads1_remedial_3_a1_1: 0/7 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/remedial/Remedial-3/Sol.java
+++ Assignments/remedial/Remedial-3/Sol.java
@@ -1,73 +1,38 @@
 import java.util.Scanner;
 import java.util.HashMap;
-/**
- * Class for student.
- */
+
 class Student {
-    /**
-     * Roll No.
-     */
+    
     private String rollNo;
-    /**
-     * Name.
-     */
+    
     private String name;
-    /**
-     * Marks.
-     */
+    
     private Double marks;
-    /**
-     * Constructs the object.
-     *
-     * @param      r     { parameter_description }
-     * @param      n     { parameter_description }
-     * @param      m     { parameter_description }
-     */
+    
     Student(final String r, final String n, final Double m) {
         this.rollNo = r;
         this.name = n;
         this.marks = m;
     }
-    /**
-     * Gets the roll no.
-     *
-     * @return     The roll no.
-     */
+    
     public String getRollNo() {
         return rollNo;
     }
-    /**
-     * Gets the name.
-     *
-     * @return     The name.
-     */
+   
     public String getName() {
         return name;
     }
-    /**
-     * Gets the marks.
-     *
-     * @return     The marks.
-     */
+    
     public Double getMarks() {
         return marks;
     }
 }
-/**
- * Class for solution.
- */
+
 final class Solution {
-    /**
-     * Constructs the object.
-     */
+    
     private Solution() {
-        //Nothing happens here.
-    }
-    /**
-     * The main function.
-     *
-     * @param      args  The arguments
-     */
+        
+  }
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
@@ -81,21 +46,21 @@ final class Solution {
         int n = sc.nextInt();
         sc.nextLine();
         for (int i = 0; i < n; i++) {
-            String[] inputs = sc.nextLine().split(" ");
+            String inputs = sc.nextLine();
             switch (inputs[2]) {
                 case "1":
-                Student s = (hm.get(inputs[1]));
+                Student s = (hm.get(inputs[2]));
                 if (s == null) {
-                    System.out.println("Student doesn't exists...");
+                    System.out.println("This marks are not awarded to any student");
                     break;
                 }
                 System.out.println(s.getName());
                 break;
                 case "2":
-                Student st = hm.get(inputs[1]);
+                Student st = hm.get(inputs[2]);
                 if (st == null) {
                     System.out.println(
-                        "Student doesn't exists...");
+                        "This marks are not awarded to any student");
                     break;
                 }
                 System.out.println(st.getMarks());
--- Assignments/remedial/Remedial-3/Solution.java
+++ Assignments/remedial/Remedial-3/Solution.java
@@ -1,27 +1,127 @@
 import java.util.Scanner;
-public final class Solution {
-    private Solution() {
+import java.util.Arrays;
 
+class Student {
+	String rollno;
+	String name;
+	Double marks;
+
+	Student() {
+
+	}
+	Student(String na, String r, Double m) {
+		this.rollno = r;
+		this.name = na;
+		this.marks = m;
+	}
+	String getRollno() {
+		return this.rollno;
+	}
+	String getName() {
+		return this.name;
+	}
+	Double getMarks() {
+		return this.marks;
+	}
+	
+public int compareTo(final Student that) {
+
+        if (this.getName() == that.getName()) {
+            return 1;
+        } else if (this.getName() == that.getName()) {
+            return -1;
+        } else {
+        if (this.getRollno() == that.getRollno()) {
+            return -1;
+        } else if (this.getRollno() == that.getRollno()) {
+            return 1;
+        } else {
+            if (this.getMarks() < that.getMarks()) {
+                return 1;
+            } else if (this.getMarks() > that.getMarks()) {
+                return -1;
+            }
+        }
+        }
+        return 0;
     }
-    public static void main(final String[] args) {
-        Scanner sc = new Scanner(System.in);
-        BinarySearchTree<String, Double> bst = new BinarySearchTree();
-        int m = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i< m; i++) {
-        	String stu = sc.nextLine();
-        	String[] data = stu.split(",");
-        	// System.out.println(data);
-        	String key = new String(data[1]);
-        	double value = Double.parseDouble(data[2]);
-        	bst.put(key, value);
-        }
-        int n = Integer.parseInt(sc.nextLine());
-        for(int i = 0; i< n; i++){
-        	String stu = sc.nextLine();
-        	System.out.println(stu);
+    class Sorting {
+        
+    private final int fifty = 50;
+    
+    private Student[] marks;
+    
+    private int size;
 
+    
+    Sorting() {
 
+        marks = new Student[fifty];
+        size = 0;
+    }
+    public void add(final Student r) {
+
+        marks[size++] = r;
+        if (size > fifty) {
+            resize();
         }
+    }
+    void resize() {
+
+        marks = Arrays.copyOf(marks, size * 2);
+    }
+    void exchange(final Student[] a, final int i, final int min) {
+
+        Student temp = a[min];
+        a[min] = a[i];
+        a[i] = temp;
+    }
+    public String toString() {
+
+        String s = "";
+        for (int i = 0; i < size; i++) {
+            s += marks[i].getName() + ",";
+        }
+        return s.substring(0, s.length() - 1);
+    }
+    public void insertionSort() {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (!less(marks, j - 1, j)) {
+                    break;
+                } else {
+                    exchange(marks, j - 1, j);
+                }
+                }
+            }
+        }
+
+    boolean less(final Student[] marks, final int j, final int min) {
+
+        return marks[j].compareTo(marks[min]) > 0;
     }
 }
+class Solution {
+    Solution() {
+    }
+    public  void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	Sorting  s = new Sorting();
+    	while (sc.hasNextLine()) {
+    		String stu = sc.nextLine();
+    		String[] data = stu.split(",");
+    		Student marks = new Student(data[0],
+                (data[1]), Double.parseDouble(data[2]));
+    		s.add(marks);
+
+    	}
+    	s.insertionSort();
+    	System.out.println(s.toString());
+
+
+    }
+
+    }
+}
+       
 
