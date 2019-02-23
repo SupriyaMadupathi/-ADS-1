d17607ec7763ee66f6fdae22250299c05a0de1b2
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2019-02-23 11:47:06 +0530

    "ADS-1 remedial-4: 7/7 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/Exam4/Question/Solution.java
+++ Assignments/Exam4/Question/Solution.java
@@ -38,7 +38,7 @@ class Solution{
 
         for (Integer each :queries ) {
             Double answer =percentiles.get(each);
-            System.out.println((answer*100.0)/100.0);
+            System.out.println(Math.round(answer*100.0)/100.0);
         }
 
         // System.out.println(percentiles);
