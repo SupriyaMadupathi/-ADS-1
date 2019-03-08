58f8dcbf5f0b4544f8382d0b38be0a1f1c536087
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2019-03-08 11:34:13 +0530

    "ADS-1 remedial-5: 7/7 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/ads1 remedial exam/Question/Solution.java
+++ Assignments/ads1 remedial exam/Question/Solution.java
@@ -1,147 +1,75 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-
-
+import java.util.*;
+import java.util.TreeSet;
+import java.util.PriorityQueue;
 class Solution{
-   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int days = Integer.parseInt(sc.nextLine());
-    // Candidate[] cad_array = new
-    CreateList cl = new CreateList();
-    int sno=0;
-    ArrayList<Candidate> cad_array = new ArrayList<>();
-    // Candidate cd =
-    while(sc.hasNext()){
-        String[] line = sc.nextLine().split(" ");
-        switch(line[0]){
-            case "Apply":
-            // System.out.println("hello ="+line[1]+"\n\n");
-                String[] tokens = line[1].split(",");
-                Candidate cd = new Candidate(tokens[0],tokens[1],tokens[2],
-                    Double.parseDouble(tokens[3]),sno++);
-                cl.addToList(cd);
-                cl.selectionSort();
-                break;
-                // cad_array.add(cd);
-            case "Recruit_Top_Candidate":
-            // System.out.println("");
-            // cl.printRest();
-            // cl.printTop();
-            // System.out.println(cl.candidate_arr[0]);
-            cl.removeTop();
-            break;
-            case "Ranking":
-            System.out.println();
-            cl.printRest();
-            break;
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		int n = Integer.parseInt(sc.nextLine());
+		MaxPQ<Student> max = new MaxPQ();
+		while(sc.hasNext()){
+			String[] data = sc.nextLine().split(" ");
+			if (data[0].equals("Apply") ) {
+				// System.out.println(data);
+				String[] s2 = data[1].split(",");
+				max.insert(new Student(Integer.parseInt(s2[0]), s2[1], s2[2], Double.parseDouble(s2[3])));
+			} else if (data[0].equals("Recruit_Top_Candidate")) {
+				System.out.println(max.delMax());
+			} else if (data[0].equals("Ranking")) {
+				System.out.println();
+				int j = 1;
+				while(!max.isEmpty()){
+					System.out.println(j+": "+max.delMax());
+					j++;
+				}
 
+				 
+			}
 
-
-        }
-    }
-  }
+			
+		}
+	}
 }
-
-class Candidate{
-    String cid,name,degree;
-    int sno;
-    Double percent;
-    Candidate(String _cid,String _name,String _degree,Double _percent,int _sno){
-        this.cid = _cid;
-        this.name = _name;
-        this.degree = _degree;
-        this.percent = _percent;
-        this.sno = _sno;
-    }
-    public String toString(){
-        return this.cid+ "," + this.name + "," + this.degree + "," +this.percent ;
-    }
-
-    public int compareTo(Candidate c){
-        if (this.percent > c.percent) {
-            return 1;
-        }
-        if (this.percent < c.percent) {
-            return -1;
-        }
-        if (this.sno > c.sno) {
-            return 1;
-        }
-        if (this.sno < c.sno) {
-            return -1;
-        }
-
-        return 0;
-    }
-}
-
-
-class CreateList{
-    Candidate[] candidate_arr;
-    int size;
-
-    CreateList() {
-        size = 0;
-        candidate_arr = new Candidate[100];
-    }
-    // void printTop(){
-    //     System.out.println(candidate_arr[0]);
-    // }
-
-    void printRest(){
-                for(int i=0;i<size-1;i++){
-                    System.out.println( i +": "+candidate_arr[i]);
-        }
-    }
-    void addToList(Candidate c) {
-        // // System.out.println("\n\n\n"+size+"\n\n\n");
-        // System.out.println("qwerfghj");
-        // System.out.println(candidate_arr[0]);
-        // System.out.println("Legn " + candidate_arr.length);
-        // candidate_arr[0] = new Candidate("10www001","Deepak","ME",92.3,2);
-        // // size++;
-
-        // // System.out.println(candidate_arr[0]);
-
-        candidate_arr[size++] = c;
-        // size++;
-    }
-
-    void removeTop(){
-                System.out.println(candidate_arr[0]);
-
-        for(int i=0;i<size-1;i++){
-            candidate_arr[i] = candidate_arr[i+1];
-            size--;
-        }
-    }
-    public String toString() {
-        String s = "";
-        for (int i = 0; i < size; i++) {
-            s += candidate_arr[i].cid + "," + candidate_arr[i].name + "," + candidate_arr[i].percent + "\n";
-        }
-        return s;
-    }
-    public void selectionSort() {
-        for (int i = 0; i < size - 1; i++) {
-            int min = i;
-            for (int j = i + 1; j < size; j++) {
-                if (less(candidate_arr, j, min)) {
-                    min = j;
-                }
-            }
-            exch(candidate_arr, i, min);
-        }
-
-    }
-    public void exch(Candidate[] candidate_arr, int i, int j) {
-        Candidate ct = candidate_arr[j];
-        candidate_arr[j] = candidate_arr[i];
-        candidate_arr[i] = ct;
-    }
-
-    boolean less(Candidate[] arr, int i, int j) {
-        return  arr[i].compareTo(arr[j]) > 0;
-    }
-
+class Student implements Comparable<Student>{
+	int roll;
+	String name;
+	String dept;
+	Double percentage;
+	Student(int r, String n, String d, Double p){
+		this.roll = r;
+		this.name = n;
+		this. dept = d;
+		this.percentage = p;
+	}
+	int getroll(){
+		return this.roll;
+	}
+	String getname(){
+		return this.name;
+	}
+	String getdept(){
+		return this.dept;
+	}
+	Double getpercent(){
+		return this.percentage;
+	}
+	public int compareTo(Student that){
+		if (this.getpercent().compareTo(that.getpercent()) > 0) {
+			return 1;
+		} else if (this.getpercent().compareTo(that.getpercent()) < 0) {
+			return -1;
+		} else{
+			
+				if (this.getroll() > that.getroll()) {
+					return -1;
+				} else if (this.getroll() < that.getroll()) {
+					return 1;
+				}
+			
+		}
+		return -1;
+	}
+	public String toString(){
+		return this.getroll()+","+this.getname()+","+this.getdept()+","+this.getpercent();
+	}
 }
